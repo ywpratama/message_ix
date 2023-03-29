979697cec52c14ff44b774e5875445e63b579da4
PRATAMA Yoga <48617743+ywpratama@users.noreply.github.com>
2023-03-29 15:30:37 +0200

    First working prototype

--- message_ix/genie_tech/__init__.py
+++ message_ix/genie_tech/__init__.py
@@ -8,46 +8,60 @@ Created on Mon Mar 20 15:41:32 2023
 import pandas as pd
 import ixmp
 import message_ix
+from message_ix.utils import make_df
 
-# A utility for copying all parameters from one scenario to another
-def add_tech( # base and scenario should be excluded, at the moment we can just read from MESSAGE initialization
-    #base,
-    #scenario=None,
+def include_tech(
+    scenario,
     technology=None,
-    parameter=None,
+    parameter=[],
     node="all",
     ):
+    
     """
     Parameters
     ----------
-    sc_ref : message_ix.Scenario()
-        MESSAGEix Scenario for copying data from.
-    sc : message_ix.Scenario()
-        MESSAGEix Scenario for copying the data to.
-    tec_ref : string
-        reference technology for copying parameters from.
-    tec : string or None, optional
-        technology for copying parameters to. if None: the same as tec_ref.
-    node_ref : string
-        reference node for copying parameters from.
-    node : string
-        node for copying parameters to.
-    par_list : list, optional
-        list of parameters to be copied. The default is 'all'.
-    par_exclude : list, optional
-        list of parameters to be excluded from being copied. The default is [].
-    remove_old : bool, optional
-        removing old data before copying new data. The default is True.
+    scenario    : message_ix.Scenario()
+        MESSAGEix Scenario where the data will be included
+    technology  : list, optional 
+        additional technologies that will be included in the model
+        the default is all technologies in the input file
+    parameter   : list, optional
+        list of parameters to be included 
+        the default is all parameters
+    node        : list, optional
+        list of nodes in which the technology is included
+        the default is all nodes
     """
 
     # Reading new technology database
-    df = pd.read_excel("tech_data.xlsx",index_col=0)
+    df = pd.read_excel("C:/Users/pratama/Documents/GitHub/MESSAGEix/message_ix/message_ix/genie_tech/tech_data.xlsx",index_col=0)
 
-
-    #if not scenario:
-    #    scenario = base
-    #scenario.check_out()
+    year_df = scenario.vintage_and_active_years()
+    vintage_years, act_years = year_df["year_vtg"], year_df["year_act"]
     
+#    if df[technology]['year_vtg'] is None:
+    yv = vintage_years
+#    else:
+#        yv = df[technology]['year_vtg']
+#    
+#    if df[technology]['year_act'] is None:
+    ya = act_years
+#    else:
+#       ya = df[technology]['year_act']
+#        
+#    
     if technology not in set(scenario.set("technology")):
         scenario.add_set("technology", technology)
-   
+            
+    for par in parameter:
+        par_data = make_df(
+            par,
+            node_loc=df[technology]['node_loc'],
+            year_vtg=yv,
+            year_act=ya,
+            time=df[technology]['time'],
+            unit="-",
+            technology=technology,
+            value=df[technology][par],
+        )
+        scenario.add_par(par, par_data)
