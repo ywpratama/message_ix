17c13efb98ae1c15129483ad5fa173fd64d9718a
PRATAMA Yoga <pratama@iiasa.ac.at>
2023-04-05 17:35:09 +0200

    update migration to yaml file

--- message_ix/genie_tech/data_in_yaml.py
+++ message_ix/genie_tech/data_in_yaml.py
@@ -6,12 +6,49 @@ Created on Tue Apr  4 15:36:19 2023
 """
 
 import yaml
+import pandas as pd
+#import ixmp
+#import message_ix
+import os
+#from message_ix.utils import make_df
 
-with open('tech_data.yml','r') as stream:
-    tech_data = yaml.safe_load(stream)
+def length(data):
+    if isinstance(data, str):
+        result = 1
+    elif isinstance(data, list):
+        result = len(data)
+    return result
 
-technology = list(tech_data.keys())
 
+with open('learning_data.yaml','r') as stream:
+    learning_data = yaml.safe_load(stream)
+
+technology = list(set(list(learning_data.keys())).difference(['size','unit']))
+print(technology)
+
+'''
 for tech in technology:
-    print(tech)
-    print(tech_data[tech])
+    data_in = learning_data.get(tech)
+    parameter = list(data_in.keys())
+    
+    for par in parameter:
+        if par == 'u':
+            value = list(data_in.get(par).values())
+            size = list(data_in.get(par).keys())
+            for v in range(len(value)):
+                par_data = make_df(
+                    par,
+                    technology=tech,
+                    value=value[v],
+                    unit='-',
+                    size=size[v])
+                print(par_data)    
+        else:
+            value = data_in.get(par)
+            par_data = make_df(
+                par,
+                technology=tech,
+                value=value,
+                unit='-')
+            print(par_data)    
+'''
